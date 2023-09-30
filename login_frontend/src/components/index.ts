6d2213931cf6c460f3f50873667024db77215204
SNXWXH <snxwxh@gmail.com>
2023-09-30 13:49:04 +0900

    ant Design 사용법 및 React TypeScript 익히는 중

---
+++ login_frontend/src/components/index.ts
@@ -0,0 +1,4 @@
+import BackgroundForm from "./signup/Background";
+import inputBox from "./signup/Input";
+
+export { BackgroundForm, inputBox };
---
+++ login_frontend/src/pages/SignUp.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../css/signup.css";
+import BackgroundForm from "../components/signup/Background";
+
+export const SignUp = () => {
+  return (
+    <>
+      <BackgroundForm />
+    </>
+  );
+};
