2d58c8b94c15a6542c6082fc1b295b384c8d891c
SNXWXH <snxwxh@gmail.com>
2023-10-02 22:35:29 +0900

    회원가입 css 수정

--- login_frontend/src/css/SignUpCss.tsx
+++ login_frontend/src/css/SignUpCss.tsx
@@ -42,7 +42,6 @@ export const signUp_signUp: React.CSSProperties = {
   fontSize: "24px",
   fontWeight: 700,
   marginBottom: "20px",
-  marginTop: "-10px",
 };
 
 export const signup_formsize: React.CSSProperties = {
